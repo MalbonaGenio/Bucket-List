1055746dd9a4beb256f9e10b0d2eedd8b70cc514
Pau Valverde <pvalverdemolina@gmail.com>
2024-11-19 07:42:41 +0900

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Pau Valverde Molina on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucket List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bucket List
+//
+//  Created by Pau Valverde Molina on 11/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
