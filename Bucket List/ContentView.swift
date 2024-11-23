39e14730407c8938b8f9cfdb5ca3ca1acee80321
Pau Valverde <pvalverdemolina@gmail.com>
2024-11-23 19:22:07 +0900

    Creating a dev branch. Moved the coordinate creation into the struct itself, not in the ContentView. Made a custom annotation that also shows sheet with location details after long press gesture on the annotation marker of the map.

--- Bucket List/ContentView.swift
+++ Bucket List/ContentView.swift
@@ -5,12 +5,54 @@
 //  Created by Pau Valverde Molina on 11/19/24.
 //
 
+import LocalAuthentication
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
+
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 35.67279, longitude: 136.69082),
+            span: MKCoordinateSpan(latitudeDelta: 17, longitudeDelta: 17))
+    )
+
     var body: some View {
-        Text("Hi")
-        Image(systemName: "map")
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                        //with iOS 18 seems to not work unless you have a minimunDuration set
+                            .onLongPressGesture(minimumDuration: 0.2) {
+                                selectedPlace = location
+                            }
+                    }
+                }
+            }
+            .mapStyle(.standard(elevation: .realistic))
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(
+                        id: UUID(), name: "New Location", description: "",
+                        latitude: coordinate.latitude,
+                        longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                //Once it runs in will unwrap the optional Location object if any, then show the view with that data. Once dissmissed it will return nil again.
+                Text(place.name)
+            }
+        }
     }
 }
 
---
+++ Bucket List/Location.swift
@@ -0,0 +1,33 @@
+//
+//  Location.swift
+//  Bucket List
+//
+//  Created by Pau Valverde Molina on 11/19/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location: Codable, Equatable, Identifiable {
+    let id: UUID
+    var name: String
+    var description: String
+    var latitude: Double
+    var longitude: Double
+
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    #if DEBUG
+    //Example location for previews
+    static var example: Location {
+        Location(id: UUID(), name: "Kyoto", description: "This is an example location of Kyoto", latitude: 35.67279, longitude: -136.69082)
+    }
+    #endif
+    
+    //If UUID is identical, two locations are the same, no need for equatable to check on all the other strcu parameters.
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+}
